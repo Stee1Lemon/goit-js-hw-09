aaea8b04c28c12104110b1a19d8dd6b2e0469312
Stee1Lemon <121914099+Stee1Lemon@users.noreply.github.com>
2023-07-21 11:05:46 +0100

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const refs = {
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+  bodyColor: document.querySelector('body'),
+};
 
+refs.btnStart.addEventListener('click', startRandomBodyColorInterval);
+refs.btnStop.addEventListener('click', stopRandomBodyColorInterval);
+
+let intervalID;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function changeBodyColor() {
+  const randomColor = getRandomHexColor();
+  refs.bodyColor.style.backgroundColor = randomColor;
+}
+
+function startRandomBodyColorInterval() {
+  refs.btnStart.disabled = true;
+    intervalID = setInterval(() => {
+      changeBodyColor();
+    }, 1000);
+}
+
+function stopRandomBodyColorInterval(playRandomColor) {
+  refs.btnStart.disabled = false;
+  clearInterval(intervalID);
+}
+
+changeBodyColor();
