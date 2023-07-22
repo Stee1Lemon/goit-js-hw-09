f0e7ba4620514ae73912c609285e6563358d8472
Stee1Lemon <121914099+Stee1Lemon@users.noreply.github.com>
2023-07-22 17:02:26 +0100

    task-2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -25,63 +25,68 @@ const options = {
 
 flatpickr('#datetime-picker', options);
 
-const timer = {
-  timerId: 0,
-  timerOn: false,
-  timeToCount: 0,
-
-  starTimer() {
-    if (this.timerOn) {
+class Timer {
+  constructor(selectedTime) {
+    this.selectedTime = selectedTime;
+    this.timerID = null;
+  }
+  start() {
+    if (this.timerID) {
       return;
     }
-    this.timerOn = true;
-    this.timerId = setInterval(() => {
-      this.timeToCount = selectedTime - Date.now();
-      const time = convertMs(this.timeToCount);
-      updateVisual(time);
-      console.log(this.timeToCount);
-      if (
-        time.days === '00' &&
-        time.hours === '00' &&
-        time.minutes === '00' &&
-        time.seconds === '00'
-      ) {
-        clearInterval(this.timerId);
+
+    this.timerID = setInterval(() => {
+      const currentTime = Date.now();
+      const timeRemaining = selectedTime - currentTime;
+
+      if (timeRemaining <= 0) {
+        this.stop();
         return;
       }
+
+      const time = this.convertMs(timeRemaining);
+      this.updateVisual(time);
     }, 1000);
-  },
-};
+  }
 
-refs.btnStart.addEventListener('click', timer.starTimer);
+  stop() {
+    clearInterval(this.timerID);
+    refs.btnStart.disabled = true;
+    this.timerID = null;
+  }
 
-function updateVisual({ days, hours, minutes, seconds }) {
-  refs.daysEl.textContent = days;
-  refs.hoursEl.textContent = hours;
-  refs.minutesEl.textContent = minutes;
-  refs.secondsEl.textContent = seconds;
+  updateVisual({ days, hours, minutes, seconds }) {
+    refs.daysEl.textContent = days;
+    refs.hoursEl.textContent = hours;
+    refs.minutesEl.textContent = minutes;
+    refs.secondsEl.textContent = seconds;
+  }
+
+  convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const days = this.pad(Math.floor(ms / day));
+    const hours = this.pad(Math.floor((ms % day) / hour));
+    const minutes = this.pad(Math.floor(((ms % day) % hour) / minute));
+    const seconds = this.pad(
+      Math.floor((((ms % day) % hour) % minute) / second)
+    );
+    return { days, hours, minutes, seconds };
+  }
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
 }
 
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+refs.btnStart.addEventListener('click', startTimer);
 
-  // Remaining days
-  const days = pad(Math.floor(ms / day));
-  // Remaining hours
-  const hours = pad(Math.floor((ms % day) / hour));
-  // Remaining minutes
-  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
-  // Remaining seconds
-  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
-  return { days, hours, minutes, seconds };
-}
-
-function pad(value) {
-  return String(value).padStart(2, '0');
+function startTimer() {
+  const timer = new Timer(selectedTime);
+  timer.start();
 }
 
 function isDateInFuture(selectedDates) {
@@ -98,3 +103,60 @@ function showUnableDateMessage() {
     timeout: 2000,
   });
 }
+
+// const timer = {
+//   timerId: 0,
+//   timerOn: false,
+//   timeToCount: 0,
+
+//   starTimer() {
+//     if (this.timerOn) {
+//       return;
+//     }
+//     this.timerOn = true;
+//     this.timerId = setInterval(() => {
+//       this.timeToCount = selectedTime - Date.now();
+//       const time = convertMs(this.timeToCount);
+//       updateVisual(time);
+//       console.log(this.timeToCount);
+//       if (
+//         time.days === '00' &&
+//         time.hours === '00' &&
+//         time.minutes === '00' &&
+//         time.seconds === '00'
+//       ) {
+//         clearInterval(this.timerId);
+//         return;
+//       }
+//     }, 1000);
+//   },
+// };
+
+// function updateVisual({ days, hours, minutes, seconds }) {
+//   refs.daysEl.textContent = days;
+//   refs.hoursEl.textContent = hours;
+//   refs.minutesEl.textContent = minutes;
+//   refs.secondsEl.textContent = seconds;
+// }
+
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   // Remaining days
+//   const days = pad(Math.floor(ms / day));
+//   // Remaining hours
+//   const hours = pad(Math.floor((ms % day) / hour));
+//   // Remaining minutes
+//   const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+//   // Remaining seconds
+//   const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+//   return { days, hours, minutes, seconds };
+// }
+
+// function pad(value) {
+//   return String(value).padStart(2, '0');
+// }
